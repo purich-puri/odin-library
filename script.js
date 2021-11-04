e4fb78f0b5c1688b03e08079ab628a12b7ebbb97
purich puri <purich.puri@gmail.com>
2021-11-04 17:51:07 +1100

    add delete button

--- script.js
+++ script.js
@@ -17,15 +17,16 @@ function closeForm() {
 
 //CONSTRUCTOR
 class Books {
-  constructor(bookName, authorName) {
+  constructor(bookID, bookName, authorName) {
+    this.bookID = bookID;
     this.bookName = bookName;
     this.authorName = authorName;
   }
 }
 
 //FUNCTION THAT USES A CONSTRUCTOR
-function addBookToLibrary(bookTitle, bookAuthor) {
-  let newBook = new Books(bookTitle, bookAuthor);
+function addBookToLibrary(bookID, bookTitle, bookAuthor) {
+  let newBook = new Books(bookID, bookTitle, bookAuthor);
   myLibrary.push(newBook);
 
   displayLibrary();
@@ -34,9 +35,10 @@ function addBookToLibrary(bookTitle, bookAuthor) {
 //GET THE INPUT VALUE FROM ID AND PASSING TO ANOTHER FUNCTION
 function submitForm() {
   const form = document.querySelector("form");
+  const randID = Date.now();
   const inputTitle = document.querySelector("#formTitle");
   const inputAuthor = document.querySelector("#formAuthor");
-  addBookToLibrary(inputTitle.value, inputAuthor.value);
+  addBookToLibrary(randID, inputTitle.value, inputAuthor.value);
 
   //CLEAR FORM
   form.reset();
@@ -48,7 +50,7 @@ function displayLibrary() {
 
   myLibrary.forEach((item) => {
     content += `<div class="card-container">
-        <div class="card" style="width: 18rem">
+        <div class="card cardLayout" style="width: 18rem">
           <div class="card-body">
             <h5 id="cardName" class="card-title">
               Name: ${item.bookName}
@@ -57,7 +59,7 @@ function displayLibrary() {
               Author: ${item.authorName}
             </h6>
           </div>
-          <button class="btn btn-danger">DELETE</button>
+          <button class="btn btn-danger" onClick="deleteBook(${item.bookID})">DELETE</button>
         </div>
       </div>`;
   });
@@ -65,8 +67,19 @@ function displayLibrary() {
   document.querySelector("#cardCollection").innerHTML = content;
 }
 
+function deleteBook(e) {
+  //MAP THE ARRAY OBJECT, SEARCH BOOKID AND RETRIEVE THE IT'S INDEX
+  let getIndex = myLibrary
+    .map(function (search) {
+      return search.bookID;
+    })
+    .indexOf(e);
+
+  myLibrary.splice(getIndex, 1);
+  displayLibrary();
+}
+
 // TODO:
-// DELETE BUTTON,
 // LOCAL STORAGE,
 // TOGGLE READ STATUS
 // CSS CLEAN UP
